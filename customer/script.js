fb045f4e358905ec189416d287bb7a96b4ec8f6f
Defolto <37343435+Defolto@users.noreply.github.com>
2019-07-19 11:02:06 +0300

    link

--- customer/script.js
+++ customer/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     if($(window).innerWidth() < 758){
         $(".block1").removeClass("px-5");
         $("h2 + p").addClass('text-center');
+        $("h2 + p + p").addClass('text-center');
         $(".block1").addClass("order-1");
         $(".block1 + div").addClass("order-0");
     }  
